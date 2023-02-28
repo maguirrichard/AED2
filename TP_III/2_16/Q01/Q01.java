b3347ef2e4e21b0145bc6214678ff9911a7ed394
Maguirrichard <42589156+maguirrichard@users.noreply.github.com>
2023-02-28 09:46:38 -0300

    Add files via upload

---
+++ TP_III/2_16/Q01/Q01.java
@@ -0,0 +1,310 @@
+  /**********************************
+    * Q01
+    * @author Maguirrichard Oliveira.
+    * @version 1 02/2016.
+    *********************************/
+class Q01
+{
+  public static void main(String[] args)
+  {
+	Pessoa pessoa = new Pessoa();
+     String str = MyIO.readLine();
+
+     while(!str.equals("FIM"))
+     {
+        pessoa.criarPessoa(str);
+	   pessoa.imprimir();
+        str = MyIO.readLine();
+     }
+  }
+}
+
+  /**********************************
+    * Pessoa
+    * @author Maguirrichard Oliveira.
+    * @version 1 02/2016.
+    *********************************/
+class Pessoa
+{
+  private int id, idade;
+  private String nome, nacionalidade, dataNasc, localNasc, dataMorte, localMorte;
+  
+  /***********************
+   * Construtor da classe.
+   ***********************/
+  public Pessoa()
+  {
+	this.id = 0;
+	this.idade = 0;
+	this.nome = " ";
+	this.nacionalidade = " ";
+	this.dataNasc = " ";
+	this.localNasc = " ";
+	this.dataMorte = " ";
+	this.localMorte = " ";
+  }
+
+  /*************************************************************************
+   * Faz a leitura do html e faz um parse para retirada dos dados da pessoa.
+   * @param nomearq String nome do arquivo a ser lido.
+   *************************************************************************/
+  public void criarPessoa (String nomearq)
+  {
+	Arq.openRead(nomearq, "UTF8");
+	int id = 0, idade = 0, coluna = 0;
+	String nome = "", nacionalidade = "", dataNasc = "", localNasc = "", 
+		  dataMorte = "", localMorte = "", tmp = "", tmp2 = "", leitor = "";
+/* --------------------------ID-----------------------------------------------------------------*/	
+	for(int i = 5; i<8; i++)
+	{
+	   tmp = tmp+nomearq.charAt(i);
+	}
+	
+	id = Integer.parseInt(tmp);
+	//MyIO.println(id);
+/* --------------------------ID-----------------------------------------------------------------*/
+
+/* --------------------------NOME---------------------------------------------------------------*/
+	for(int i = 1; i <= 38; i++) leitor = Arq.readLine();
+      
+	//MyIO.println(leitor);
+	//MyIO.println(coluna);
+	coluna = 56;
+	while((int)leitor.charAt(coluna) != 60)
+	{//Inicio WHILE
+	   nome = nome+leitor.charAt(coluna);
+	   //MyIO.println(help + " " + coluna);
+ 	   coluna++;
+	}//Fim WHILE
+
+	//nome = pegaNome(str, 38, 56, 60);
+	//MyIO.println(nome);
+/* --------------------------NOME---------------------------------------------------------------*/
+
+/* --------------------------NACIONALIDADE------------------------------------------------------*/
+	tmp = "Nacionalidade";
+
+	while(!leitor.contains(tmp)) leitor = Arq.readLine();
+
+     nacionalidade = removeTags(Arq.readLine());
+	//MyIO.println(nacionalidade);
+/* --------------------------NACIONALIDADE------------------------------------------------------*/
+
+/* --------------------------DATANASC-----------------------------------------------------------*/
+	tmp = "Nascimento";
+
+	while(!leitor.contains(tmp)) leitor = Arq.readLine();
+
+     dataNasc = removeTags(Arq.readLine());
+	if(dataNasc.charAt(dataNasc.length()-1) == ')')
+	{
+	   int k = dataNasc.indexOf('(') + 1;
+	   //MyIO.println(k);
+
+	   for(int i = k; i < k+2; i++)
+	      tmp2 = tmp2+dataNasc.charAt(i);
+
+	   idade = Integer.parseInt(tmp2);
+	   dataMorte = "vivo";
+	   localMorte = "vivo";
+	   dataNasc = dataNasc.substring(0, dataNasc.length()-9);
+	}
+	//MyIO.println(dataNasc);
+/* --------------------------DATANASC-----------------------------------------------------------*/
+
+/* --------------------------LOCALNASC----------------------------------------------------------*/
+	tmp = "Local";
+
+	while(!leitor.contains(tmp)) leitor = Arq.readLine();
+
+     localNasc = removeTags(Arq.readLine());
+	//MyIO.println(localNasc);
+/* --------------------------LOCALNASC----------------------------------------------------------*/
+
+/* -------------------------DATAMORTE-----------------------------------------------------------*/
+	tmp = "Morte";
+
+
+	if(dataMorte.length() == 0)
+	{
+	   while(!leitor.contains(tmp)) leitor = Arq.readLine();
+
+	   dataMorte = removeTags(Arq.readLine());
+	   if(dataMorte.charAt(dataMorte.length()-1) == ')')
+	   {
+	      int k = dataMorte.indexOf('(') + 1;
+	      //MyIO.println(k);
+	      tmp2 = "";
+
+	      for(int i = k; i < k+2; i++)
+	        tmp2 = tmp2+dataMorte.charAt(i);
+
+	      idade = Integer.parseInt(tmp2);
+	      dataMorte = dataMorte.substring(0, dataMorte.length()-9);
+	   }
+	}
+
+	//MyIO.println(dataMorte);
+/* -------------------------DATAMORTE-----------------------------------------------------------*/
+
+/* -------------------------LOCALMORTE----------------------------------------------------------*/
+	tmp = "Local";
+
+	if(localMorte.length() == 0)
+	{
+	   while(!leitor.contains(tmp)) leitor = Arq.readLine();
+
+	   localMorte = removeTags(Arq.readLine());
+	}
+
+	//MyIO.println(localMorte);
+/* -------------------------LOCALMORTE----------------------------------------------------------*/
+
+	Arq.close();   
+	
+/* -------------------------FAZ_PESSOA----------------------------------------------------------*/
+	
+	this.id = id;
+	this.nome = nome;
+	this.nacionalidade = nacionalidade;
+	this.dataNasc = dataNasc;
+	this.localNasc = localNasc;
+	this.dataMorte = dataMorte;
+	this.localMorte = localMorte;
+	this.idade = idade;
+
+/* -------------------------FAZ_PESSOA----------------------------------------------------------*/	
+  }
+
+  /****************************************************
+   * Retira as tags de cada linha lida no arquivo html.
+   * @param s String linha do arquivo a ser tratada.
+   ****************************************************/
+  public String removeTags(String s)
+  {
+	String resp = "";
+	 
+	for (int i = 0; i < s.length(); i++)
+	{
+	 
+	//Perguntas: (1) Pq o while abaixo? (2) Pq as clausulas do while abaixo nao podem ser invertidas?
+	   while(i < s.length() && s.charAt(i) == '<')
+	   {
+	      for (i++; s.charAt(i) != '>'; i++);
+	      i++;
+	 
+	      //Pergunta: (1) Qual seria um exemplo da utilidade do laco abaixo?
+	      while(i < s.length() && s.charAt(i) == '&')
+	      {
+	         for (i++; s.charAt(i) != ';'; i++);
+	         i++;
+	      }
+	   }
+	 
+	   //Pergunta: (1) Qual seria um exemplo da utilidade do laco abaixo?
+	   while(i < s.length() && s.charAt(i) == '&')
+	   {
+	      for (i++; s.charAt(i) != ';'; i++);
+	      i++;
+	      resp += ' ';
+	   }
+	 
+	   //Perqunta: Pq nao colocamos apenas resp += s.charAt(i)?
+	   if(i < s.length()) resp += s.charAt(i);
+         
+     }
+ 
+	while(resp.length() > 0 && resp.charAt(0) == ' ') resp = resp.substring(1);
+ 
+	return resp;
+  }
+
+  public void imprimir(){
+	MyIO.println(this.id + " " + this.nome  + " " + this.nacionalidade + " " +  this.dataNasc + " " 
+                  + this.localNasc + " " + this.dataMorte + " " + this.localMorte + " " + this.idade);
+  }
+
+  /*               Metodos SET                 */
+
+  public void setID (int id)
+  {
+	this.id = id;
+  }
+
+  public void setIdade (int idade)
+  {
+	this.idade = idade;
+  }
+
+  public void setNome (String nome)
+  {
+	this.nome = nome;
+  }
+
+  public void setNacionalidade (String nacionalidade)
+  {
+	this.nacionalidade = nacionalidade;
+  }
+
+  public void setDataNasc (String dataNasc)
+  {
+	this.dataNasc = dataNasc;
+  }
+
+  public void setLocalNasc (String localNasc)
+  {
+	this.localNasc = localNasc;
+  }
+
+  public void setDataMorte (String dataMorte)
+  {
+	this.dataMorte = dataMorte;
+  }
+
+  public void setLocalMorte (String localMorte)
+  {
+	this.localMorte = localMorte;
+  }
+
+  /*               Metodos GET                 */
+
+  public int getID ()
+  {
+	return this.id;
+  }
+
+  public int getIdade ()
+  {
+	return this.idade = idade;
+  }
+
+  public String getNome ()
+  {
+	return this.nome;
+  }
+
+  public String getNacionalidade ()
+  {
+	return this.nacionalidade;
+  }
+
+  public String getDataNasc ()
+  {
+	return this.dataNasc;
+  }
+
+  public String getLocalNasc ()
+  {
+	return this.localNasc;
+  }
+
+  public String getDataMorte ()
+  {
+	return this.dataMorte;
+  }
+
+  public String getLocalMorte ()
+  {
+	return this.localMorte;
+  }
+}
